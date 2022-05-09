cb753d4b742b77b50e15141b38894b083d640e67
Nitheesh S <nitheesh.s@tringapps.com>
2022-05-09 20:00:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import AddDetail from './Components/AddDetail';
+import ShowAllDetails from './Components/ShowAllDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<AddDetail/>}/>
+        <Route path='showdetails' element={<ShowAllDetails/>}/>
+      </Routes>
     </div>
   );
 }
